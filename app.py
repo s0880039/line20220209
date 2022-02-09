2769b1d16d737dc58ccc8134fc017339cdaefea4
s0880039 <99315731+s0880039@users.noreply.github.com>
2022-02-09 20:51:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -240,9 +240,10 @@ def getTotalSentMessageCount():
 
 
 def getTodayCovid19Message():
-    date = ""
-    total_count = 0
-    count = 0
+    response = requests.get("https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=4001&limited=TWN")
+    date = response.json()[0]["a04"]
+    total_count = response.json()[0]["a05"]
+    count = response.json()[0]["a06"]
     return F"日期：{date}, 人數：{count}, 確診總人數：{total_count}"
 
 

cab0f76744666cdfa06af793740aa1c08fac01ab
s0880039 <99315731+s0880039@users.noreply.github.com>
2022-02-10 20:56:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -221,7 +221,7 @@ def getCarouselMessage(data):
 
 
 def getLocationConfirmMessage(title, latitude, longitude):
-    data={"title":title, "latitude":latitude, "longitude":longitude, "action": "get_near"}
+    data = {'title':title, 'latitude':latitude, 'longitude':longitude, 'action': 'get_near'}
     message = {
               "type": "template",
               "altText": "this is a confirm template",

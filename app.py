df46c8b70dde3a6739e6850c73fa940d3ee4f741
s0880039 <99315731+s0880039@users.noreply.github.com>
2022-02-10 14:16:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,48 +45,7 @@ def index():
                 text = events[0]["message"]["text"]
 
                 if text == "我的名字":
-                    payload["messages"] = [{
-                                            "type": "text",
-                                            "text": "$$$$$$$",
-                                            "emojis": [
-                                              {
-                                                "index": 0,
-                                                "productId": "5ac21a8c040ab15980c9b43f",
-                                                "emojiId": "023"
-                                              },
-                                              {
-                                                "index": 1,
-                                                "productId": "5ac21a8c040ab15980c9b43f",
-                                                "emojiId": "035"
-                                              },
-                                              {
-                                                "index": 2,
-                                                "productId": "5ac21a8c040ab15980c9b43f",
-                                                "emojiId": "038"
-                                              },
-                                              {
-                                                "index": 3,
-                                                "productId": "5ac21a8c040ab15980c9b43f",
-                                                "emojiId": "038"
-                                              },
-                                              {
-                                                "index": 4,
-                                                "productId": "5ac21a8c040ab15980c9b43f",
-                                                "emojiId": "035"
-                                              },
-                                              {
-                                                "index": 5,
-                                                "productId": "5ac21a8c040ab15980c9b43f",
-                                                "emojiId": "027"
-                                              },
-                                              {
-                                                "index": 6,
-                                                "productId": "5ac21a8c040ab15980c9b43f",
-                                                "emojiId": "039"
-                                              }
-                                            ]
-                                        }
-                                         ]
+                    payload["messages"] = [getNameEmojiMessage()]
                 elif text == "出去玩囉":
                     payload["messages"] = [getPlayStickerMessage()]
                 elif text == "台北101":
@@ -209,8 +168,20 @@ def sendTextMessageToMe():
 def getNameEmojiMessage():
     lookUpStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     productId = "5ac21a8c040ab15980c9b43f"
-    name = "william"
+    name = "Miles"
     message = dict()
+    message["type"] = "text"
+    message["text"] = "".join("$" for r in range(len(name)))
+    emojis_list = list()
+    for i, nChar in enumerate(name):
+        emojis_list.append(
+            {
+              "index": i,
+              "productId": productId,
+              "emojiId": f"{lookUpStr.index(nChar) + 1 :03}"
+            }
+        )
+    message["emojis"] = 
     return message
 
 
